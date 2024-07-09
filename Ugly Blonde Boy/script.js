72d6153803d7edc7c2ab5577011756f8d6157688
kriqk <kristakojdhelaj@gmail.com>
2024-07-09 15:34:05 +0300

    Update script.js

---
+++ Ugly Blonde Boy/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const head = document.querySelector('.head');
+    head.addEventListener('click', () => {
+        head.style.backgroundColor = 'green';
+    });
+});
